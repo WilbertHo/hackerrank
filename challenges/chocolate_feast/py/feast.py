0825b348734400e5f79886a8dc850e4a7b28dcd5
Wil Ho <wilho@wetafx.co.nz>
2014-02-11 17:14:40 +1300

    [chocolate feast] (py) Algorithm fix.

--- challenges/chocolate_feast/py/feast.py
+++ challenges/chocolate_feast/py/feast.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 
-def main():
-    credit = 10
-    cost = 2
-    total = 0
+def calcCandies(credit, cost, wrapper_cost):
+    total = (credit / cost)
 
+    credit = (credit / cost)
+    cost = wrapper_cost
     while(credit >= cost):
         total += credit / cost
         leftover = (credit / cost) + (credit % cost)
         credit = leftover
 
-    print total
+    return total
+
+def main():
+    # Total candies == credit/cost + wrappers/wrapper_cost
+    credit = 10
+    cost = 2
+    wrapper_cost = 5
+
+    print calcCandies(credit, cost, wrapper_cost)
+
+    print calcCandies(12, 4, 4)
+    print calcCandies(6, 2, 2)
 
 if __name__ == '__main__':
     main()
