02af3d2d57b365a499d863c0e53197a9d8d98fb0
Wil Ho <wilho@wetafx.co.nz>
2014-02-12 14:24:55 +1300

    [chocolate feast] (scala) Takes input from stdin or file.

--- challenges/chocolate_feast/scala/src/feast.scala
+++ challenges/chocolate_feast/scala/src/feast.scala
@@ -1,10 +1,13 @@
+import scala.annotation.tailrec
+
 class Feast {
 
-  def feast(credit: Int, cost: Int, total: Int): Int = {
+  @tailrec
+  final def feast(credit: Int, cost: Int, total: Int): Int = {
     if (credit < cost) return total;
-    total = total + (credit / cost);
-    val leftover = (credit / cost) + (credit % cost);
-    return feast(leftover, cost, total);
+    return feast((credit / cost) + (credit % cost),
+                  cost,
+                  total + (credit / cost));
   }
 
 }
@@ -12,6 +15,17 @@ class Feast {
 object Main {
 
   def main(args: Array[String]) {
+    // get input
+    val input = if (args.length < 1) io.Source.stdin
+                else io.Source.fromFile(args(0));
+    val f = new Feast();
+
+    for (line <- input.getLines()) {
+      val Array(dollars, costDollar, costWrapper) =
+        line.trim().split("""\s+""").map(x => x.toInt);
+      val candies = dollars / costDollar;
+      println(f.feast(candies, costWrapper, candies));
+    }
   }
 
 }
