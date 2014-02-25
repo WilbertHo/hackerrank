6f242983262877b9e1213e0c887f1c06e5e49163
Wil Ho <wilho@wetafx.co.nz>
2014-02-25 18:33:05 +1300

    [chocolate feast] (c++) Working implementation.

--- challenges/chocolate_feast/cpp/src/feast.cpp
+++ challenges/chocolate_feast/cpp/src/feast.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 
 using std::cin;
 using std::cout;
 using std::endl;
-using std::string;
+using std::getline;
 using std::istream;
 using std::ifstream;
-using std::getline;
+using std::string;
+
+int redeem_wrappers(int credit, int cost) {
+  if (cost > credit) return 0;
+  int wrappers = credit / cost;
+  int leftover = credit % cost;
+  return wrappers + redeem_wrappers(wrappers + leftover, cost);
+}
 
 int main(int argc, char* argv[]) {
   string mystr;
   istream * input;
   ifstream in_file;
 
+  // Read from file if given, else STDIN
   if (argc == 2) {
     in_file.open(argv[1]);
     input = &in_file;
@@ -28,7 +37,12 @@ int main(int argc, char* argv[]) {
   getline(*input, mystr);
 
   while (getline(*input, mystr)) {
-    cout << mystr << endl;
+    std::istringstream iss(mystr);
+    int credit, price, wrapper_price;
+    iss >> credit >> price >> wrapper_price;
+    int wrappers = credit / price;
+    wrappers += redeem_wrappers(wrappers, wrapper_price);
+    cout << wrappers << endl;
   }
 
   return 0;
