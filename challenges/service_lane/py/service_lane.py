f3b53162155230236a3e8b5929cc99793e570f7a
Wilbert Ho <wilbert.ho@gmail.com>
2014-06-03 23:15:29 +1200

    [service lane] Initial Python.

---
+++ challenges/service_lane/py/service_lane.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import fileinput
+
+def main():
+    # read input
+    _input = [line.strip() for line in fileinput.input()]
+
+    # first line: length of road, num test cases
+    _input.pop(0)
+    # second line: array representing road
+    road = [int(width) for width in _input.pop(0).split()]
+
+    for case in _input:
+        enter, exit = [int(width) for width in case.split()]
+        print enter, exit
+
+if __name__ == '__main__':
+    main()
