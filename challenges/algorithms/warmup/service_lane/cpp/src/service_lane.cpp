50c5947c6bbb47702ced3d26c89b40cb5dfcd96a
Wil Ho <wilho@wetafx.co.nz>
2014-06-10 18:18:09 +1200

    const auto to shared_ptr test.

--- challenges/algorithms/warmup/service_lane/cpp/src/service_lane.cpp
+++ challenges/algorithms/warmup/service_lane/cpp/src/service_lane.cpp
@@ -22,6 +22,15 @@ shared_ptr<vector<vector<T>>> process_input(istream* is) {
     }
     input->push_back(current_line);
   }
+
+  for (auto line: *input) {
+    for (auto temp: line) {
+      cout << "hi ";
+    }
+    cout << endl;
+  }
+
+  // return the pointer (address)
   return input;
 }
 
@@ -36,24 +45,28 @@ int main(int argc, char* argv[]) {
   } else {
     input = &cin;
   }
+
+  // receive a pointer (address) from process_input
   // vector<vector<int>>* v = process_input<int>(input);
-  auto v = process_input<int>(input);
+  // const vector<vector<int>>& v = *process_input<int>(input);
+  const auto& v = *process_input<int>(input);
 
   // first line gives the length of the road and number of cases
   // int road_len = *(v->begin()->begin());
   // int num_cases = *((v->begin()->begin())+1);
-  auto road_len = *(v->begin()->begin());
-  auto num_cases = *((v->begin()->begin())+1);
+  const auto& road_len = v.begin()->begin();
+  const auto& num_cases = (v.begin()->begin()) + 1;
 
   // second line describes width of the road
   // vector<int> road = *(v->begin()+1);
-  auto road = *(v->begin()+1);
+  const auto& road = v.begin() + 1;
 
   // rest are cases
   // for (vector<vector<int>>::iterator temp = v->begin() + 2; temp != v->end(); temp++) {
-  for (auto temp = v->begin() + 2; temp != v->end(); temp++) {
+  for (auto temp = v.begin() + 2; temp != v.end(); temp++) {
+    int min_width = 0;
     // for (vector<int>::iterator foo = temp->begin(); foo != temp->end(); foo++) {
-    for (auto foo: *temp) {
+    for (const auto& foo: *temp) {
       cout << foo << " ";
     }
     cout << endl;
