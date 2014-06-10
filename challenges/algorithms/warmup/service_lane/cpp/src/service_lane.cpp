e92fcd402383749db595d4dd236793b65563e543
Wilbert Ho <wilbert.ho@gmail.com>
2014-06-11 00:39:03 +1200

    [service lane] (c++) Fix shared_ptr lifetime issues.

--- challenges/algorithms/warmup/service_lane/cpp/src/service_lane.cpp
+++ challenges/algorithms/warmup/service_lane/cpp/src/service_lane.cpp
@@ -8,9 +8,7 @@
 using namespace std;
 
 template<typename T>
-// vector<vector<T>>* process_input(istream* is) {
 shared_ptr<vector<vector<T>>> process_input(istream* is) {
-  // vector<vector<T>>* input = new vector<vector<T>>;
   shared_ptr<vector<vector<T>>> input(new vector<vector<T>>);
   string buffer;
   while (getline(*is, buffer)) {
@@ -21,13 +19,6 @@ shared_ptr<vector<vector<T>>> process_input(istream* is) {
       current_line.push_back(token);
     }
     input->push_back(current_line);
-  }
-
-  for (auto line: *input) {
-    for (auto temp: line) {
-      cout << "hi ";
-    }
-    cout << endl;
   }
 
   // return the pointer (address)
@@ -47,27 +38,32 @@ int main(int argc, char* argv[]) {
   }
 
   // receive a pointer (address) from process_input
-  // vector<vector<int>>* v = process_input<int>(input);
-  // const vector<vector<int>>& v = *process_input<int>(input);
-  const auto& v = *process_input<int>(input);
+  const auto& v = process_input<int>(input);
+
+  /*
+  for (const auto line: *v) {
+    for (const auto temp: line)
+      cout << temp << " ";
+    cout << endl;
+  }
+  */
 
   // first line gives the length of the road and number of cases
-  // int road_len = *(v->begin()->begin());
-  // int num_cases = *((v->begin()->begin())+1);
-  const auto& road_len = v.begin()->begin();
-  const auto& num_cases = (v.begin()->begin()) + 1;
+  // int road_len
+  // int num_cases
+  const auto& road_len = v->begin()->begin();
+  const auto& num_cases = (v->begin()->begin()) + 1;
 
   // second line describes width of the road
-  // vector<int> road = *(v->begin()+1);
-  const auto& road = v.begin() + 1;
+  const auto& road = v->begin() + 1;
 
   // rest are cases
   // for (vector<vector<int>>::iterator temp = v->begin() + 2; temp != v->end(); temp++) {
-  for (auto temp = v.begin() + 2; temp != v.end(); temp++) {
+  for (auto line = v->begin() + 2; line != v->end(); line++) {
     int min_width = 0;
-    // for (vector<int>::iterator foo = temp->begin(); foo != temp->end(); foo++) {
-    for (const auto& foo: *temp) {
-      cout << foo << " ";
+    // for (vector<int>::iterator foo = line->begin(); foo != line->end(); foo++) {
+    for (const auto& token: *line) {
+      cout << token << " ";
     }
     cout << endl;
   }
