c440abc84b2a3df3bb6d8cc30510d3f62479b5fe
Wil Ho <wilho@wetafx.co.nz>
2014-06-13 13:54:41 +1200

    [service lane] (scala Submitted version.

--- challenges/algorithms/warmup/service_lane/scala/src/service_lane.scala
+++ challenges/algorithms/warmup/service_lane/scala/src/service_lane.scala
@@ -1,6 +1,29 @@
-/**
- * Created by wil on 6/11/14.
- */
-class service_lane {
+import scala.annotation.tailrec
+
+object Solution {
+
+  // def processLine(lines: Iterator[String]): Array[Int] = {
+  //   return lines.next().split("""\s+""").map(x => x.toInt);
+  // }
+
+  def processLine(lines: Iterator[String]): List[Int] = {
+    return lines.next().split("""\s+""").map(x => x.toInt).toList;
+  }
+
+  def main(args: Array[String]) {
+    // get input
+    val input = if (args.length < 1) io.Source.stdin
+                else io.Source.fromFile(args(0));
+    val lines = input.getLines();
+
+    val List(road_length, num_cases) = processLine(lines);
+    val road = processLine(lines);
+
+    while (lines.hasNext) {
+      val List(entry, exit) = processLine(lines);
+      val min = road.slice(entry, exit + 1).min;
+      println(min);
+    }
+  }
 
 }
