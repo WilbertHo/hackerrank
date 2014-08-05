bb22ffed913d36b65bda11b45939f1f5cc987389
Wil Ho <wilho@wetafx.co.nz>
2014-08-05 20:34:45 +1200

    TODO: case match looks for List(a, b, c) which only works for a winSize of 3. Generalize.

--- challenges/algorithms/warmup/angry_children/scala/src/main/scala/angry_children.scala
+++ challenges/algorithms/warmup/angry_children/scala/src/main/scala/angry_children.scala
@@ -1,8 +1,13 @@
 object AngryChildren {
   def main(args: Array[String]) {
-    // get input
-    val input = if (args.length < 1) io.Source.stdin
-                else io.Source.fromFile(args(0));
-    println(input.getLines.toList)
+    // get input. if no args passed, then take stdin, else read the
+    // passed arg as a filename
+    val input = (if (args.length < 1) io.Source.stdin
+                 else io.Source.fromFile(args(0))).getLines.map(x => x.toInt).toList
+
+    println(input.sorted)
+
+    val winSize = input(1)
+    println(input.slice(2, input.size).sorted.sliding(winSize).map{ case List(a, b, c) => c - a }.min)
   }
 }
