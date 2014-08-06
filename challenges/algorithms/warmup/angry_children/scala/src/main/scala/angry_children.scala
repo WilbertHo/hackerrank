e90b50bd06e7faf99fd54157f8ee23e1e01080fa
Wilbert Ho <wilbert.ho@gmail.com>
2014-08-06 23:31:44 +1200

    [angry children] (scala) Tail recursive version.

--- challenges/algorithms/warmup/angry_children/scala/src/main/scala/angry_children.scala
+++ challenges/algorithms/warmup/angry_children/scala/src/main/scala/angry_children.scala
@@ -1,4 +1,16 @@
+import scala.annotation.tailrec
+
 object AngryChildren {
+
+  @tailrec
+  def findMin(cases: Seq[Int], winSize: Int=0, minSoFar: Int=0): Int = {
+    // base case
+    if (cases.size < winSize) return minSoFar
+
+    val unfairness = cases(winSize - 1) - cases(0)
+    return findMin(cases.slice(1, cases.size), winSize, if (unfairness < minSoFar) unfairness else minSoFar)
+  }
+
   def main(args: Array[String]) {
     // Get input.  Drop the first item, which is number of cases.
     val input = (if (args.length < 1) io.Source.stdin
@@ -6,20 +18,8 @@ object AngryChildren {
 
     // Window size is the next item
     val winSize = input(0)
-
-    var minUnfairness = input.max
     val cases = input.drop(1).sorted
 
-    // Scala's sliding method's iterator is too slow.
-    // input.drop(1).sorted.sliding(winSize).map(x => if (x(winSize - 1) - x(0) < minUnfairness) minUnfairness = x(winSize-1) - x(0))
-    for (begin <- 0 until cases.size - winSize) {
-      val left = cases(begin)
-      val right = cases(begin + winSize - 1)
-      if (right - left < minUnfairness) {
-        minUnfairness = right - left
-      }
-    }
-
-    println(minUnfairness)
+    println(findMin(cases, winSize, cases.max))
   }
 }
