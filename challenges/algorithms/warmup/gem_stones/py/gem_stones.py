966335d7147f114d42a9a00ba3e659dacb5775f2
Wilbert Ho <wilbert.ho@gmail.com>
2014-09-01 00:54:11 +1200

    [gem stones] (py) Submitted version.

--- challenges/algorithms/warmup/gem_stones/py/gem_stones.py
+++ challenges/algorithms/warmup/gem_stones/py/gem_stones.py
@@ -2,9 +2,21 @@
 
 import fileinput
 
+
 def main():
     input = [line.strip() for line in fileinput.input()]
-    print "hi"
+
+    num_cases = input.pop(0)
+    gems = set(input[0])
+
+    for rock in input:
+        elements = set()
+        for element in rock:
+            elements.add(element)
+        gems = gems.intersection(elements)
+
+    print len(gems)
+
 
 if __name__ == '__main__':
     main()
