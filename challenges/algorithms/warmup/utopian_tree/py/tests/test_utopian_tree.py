73e9a0cb3c752f97e62fffe8b253e6ec019dec79
Wilbert Ho <wilbert.ho@gmail.com>
2015-01-21 19:01:50 +1300

    [utopian tree] (python) Fix test module that was using wrong class/method name.

--- challenges/algorithms/warmup/utopian_tree/py/tests/test_utopian_tree.py
+++ challenges/algorithms/warmup/utopian_tree/py/tests/test_utopian_tree.py
@@ -2,7 +2,11 @@
 
 from nose.tools import eq_
 
-from utopiantree import UtopianTree
+from utopiantree import calc_height
 
 def test_calc_height():
-
+    eq_(calc_height(0), 1)
+    eq_(calc_height(1), 2)
+    eq_(calc_height(2), 3)
+    eq_(calc_height(3), 6)
+    eq_(calc_height(4), 7)
