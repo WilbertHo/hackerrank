7307ffff2990123824862049d4e01c1eb5755414
Wilbert Ho <wilbert.ho@gmail.com>
2015-01-28 18:23:05 +1300

    [cavity map] (python) Submitted version. Passes all tests.

--- challenges/algorithms/implementation/cavity_map/py/cavitymap.py
+++ challenges/algorithms/implementation/cavity_map/py/cavitymap.py
@@ -1,6 +1,34 @@
-class CavityMap(object):
-    def __init__(self):
-        pass
+import fileinput
 
-    def map_cavities(self, grid):
-        pass
+
+def map_cavities(grid):
+    def is_cavity(row, col):
+        depth = grid[row][col]
+        if grid[row - 1][col] < depth and \
+           grid[row + 1][col] < depth and \
+           grid[row][col - 1] < depth and \
+           grid[row][col + 1] < depth:
+            return True
+        return False
+
+    cavity_map = grid
+
+    for row in range(1, len(grid) - 1):
+        for col in range(1, len(grid[0]) - 1):
+            if is_cavity(row, col):
+                cavity_map[row] = cavity_map[row][:col] + 'X' + cavity_map[row][col + 1:]
+
+    return cavity_map
+
+
+def main():
+    # get input grid, discard first line, which is number of lines
+    input = [line.strip() for line in fileinput.input()][1:]
+
+    cavities = map_cavities(input)
+    for row in cavities:
+        print row
+
+
+if __name__ == '__main__':
+    main()
--- challenges/algorithms/implementation/cavity_map/py/tests/test_cavitymap.py
+++ challenges/algorithms/implementation/cavity_map/py/tests/test_cavitymap.py
@@ -1,6 +1,6 @@
-from nose.tests import eq_
-from cavitymap import CavityMap
+from nose.tools import eq_
+from cavitymap import map_cavities
 
 
 def test_cavitymap():
-    eq_(CavityMap.map_cavities(['1112', '1912', '1892', '1234']), ['1112', '1X12', '18X2', '1234'])
+    eq_(map_cavities(['1112', '1912', '1892', '1234']), ['1112', '1X12', '18X2', '1234'])
